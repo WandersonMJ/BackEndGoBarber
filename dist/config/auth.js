36e4721dc04bc17fcc1c0882a08f1f552dcc9236
WandersonMJ <wanderson.marques@fatecitapetininga.edu.br>
2021-02-08 21:56:16 -0300

    master: saving issues

---
+++ dist/config/auth.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    // abacate
+    jwt: {
+        secret: '04b6e1a104ba0ed5e7985abde3e13140',
+        expiresIn: '1d',
+    },
+};
